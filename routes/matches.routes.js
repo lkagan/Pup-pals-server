df6bc7e922f153662ab7d162dd8fe8c94a3c3439
Larry Kagan <larry@superiocity.com>
2022-06-24 19:04:03 -0400

    Filter out users own dog from random search

--- routes/matches.routes.js
+++ routes/matches.routes.js
@@ -8,13 +8,16 @@ router.get("/", async (req, res, next) => {
         const usersDog = await Dog.findOne({ user: req.user._id });
 
         // Get the dogs' IDs that the owner likes
-        const likedViews = await View.find({liked: true, viewed_for: usersDog._id});
+        const likedViews = await View.find({
+            liked: true,
+            viewed_for: usersDog._id
+        });
 
         // Get an array of liked dogs ID's
         const likedDogsIds = likedViews.map(dog => dog.viewed_by.toString());
 
         // Get the liked dogs
-        const likedDogs = await Dog.find({ _id: { $in: likedDogsIds}})
+        const likedDogs = await Dog.find({ _id: { $in: likedDogsIds } })
 
         // TODO: Filter liked dogs to those who also liked the user's dog.
 
@@ -25,23 +28,28 @@ router.get("/", async (req, res, next) => {
 
 })
 
-router.get("/find", (req, res, next) => {
-    // TODO: filter out own dog and dogs that have already been viewed
-    Dog.aggregate([
-        { $sample: { size:  1 }},
-    ])
-    .then((dogs) => {
+router.get("/find", async (req, res, next) => {
+    // TODO: filter out dogs that have already been viewed
+    try {
+        const usersDog = await Dog.findOne({ user: req.user._id });
+
+        // Don't include the user's own dog
+        const dogs = await Dog.aggregate([
+            { $match: { _id: { $ne: usersDog._id } } },
+            { $sample: { size: 1 } }
+        ]);
+
         res.status(200).json(dogs[0]);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 })
 
 router.post("/like", async (req, res, next) => {
     console.log('reviewed', req.body);
     try {
-        dog = await Dog.findOne({user: req.user._id});
+        dog = await Dog.findOne({ user: req.user._id });
 
         await View.create({
             viewed_by: dog._id,
